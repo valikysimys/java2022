15a7bce559eab602a12ed3766f076ff30148c1ff
valikysimys <62375359+valikysimys@users.noreply.github.com>
2022-11-14 21:02:30 +0200

    Add files via upload

---
+++ lab4/src/main/java/com/example/lab4/ShopFrame.java
@@ -0,0 +1,23 @@
+package com.example.lab4;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ShopFrame extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ShopFrame.class.getResource("shop.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 556, 388);
+        stage.setTitle("To4ka");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
